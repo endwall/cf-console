c60136c199fdf7450d7e55b71b5935e1d25e5a0c
endwall <endwall@gmail.com>
2013-07-17 12:53:46 +1000

    cloud foundry web ui initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfConsole::Application.config.secret_key_base = 'd51a941c6edd431e6c50f0f7617fca18c67e5f3ee156426426f20b0fea5e8413d8c6c28247cbeb5f4ad2d0c05423b133c01ce83d4762c00d487f18467e2a9287'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CfConsole::Application.config.session_store :cookie_store, key: '_cf-console_session'
